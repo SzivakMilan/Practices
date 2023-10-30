23fa2d5962bc407ca0118f700628853286ef546e
SzivakMilan <122488660+SzivakMilan@users.noreply.github.com>
2023-10-30 09:02:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,146 @@
+# Description
+You have to complete 4 functions, in 4 different files:  
+- sumOfTwoNumbers -> `sumOfTwoNumbers.js`  
+- repeatMyself -> `repeatMyself.js`  
+- numberChecking -> `numberChecking.js`
+- findAGoodCar -> `findAGoodCar.js`
+
+Only write in the listed js files. Do not change any starter code.
+
+Each function should return a value, depending on the task. 
+
+You can run some built-in tests with the `node test.js` command.  
+You can run the js files independently with the `node filename.js` command.
+
+---
+
+# sumOfTwoNumbers
+Write a function, that has number 2 parameters: `num1` and `num2`.  
+The function should return the sum of the 2 parameters.  
+The function always gets 2 numbers for arguments, no need to check those.
+
+examples:
+
+`sumOfTwoNumbers(1, 2)` should return `3`  
+`sumOfTwoNumbers(-2, -4)` should return `-6`
+
+---
+
+# repeatMyself
+Write a function that has 1 string parameter: `string`.  
+You should return the string 5 times, concatenated, with increasing amount of exclamation marks added at the end of each sentence.  
+It should work with empty strings, too.
+
+examples:
+
+`repeatMyself("hello")` should return `"hello!hello!!hello!!!hello!!!!hello!!!!!"`  
+`repeatMyself("hi!")` should return `"hi!!hi!!!hi!!!!hi!!!!!hi!!!!!!"`  
+`repeatMyself("")` should return `"!!!!!!!!!!!!!!!"`
+
+---
+
+# numberChecking
+Write a function that has 1 parameter: `number`.  
+If the value of the parameter is a number that is greater than 10, you should return `"sok"`.  
+If the value of the parameter is a negative number, you should return `0`.  
+If the value of the parameter is not a whole number, you should return that number rounded down.
+
+examples:
+
+`numberChecking(15)` should return `"sok"`  
+`numberChecking(4.5)` should return `4`  
+`numberChecking(-111)` should return `0`
+
+---
+
+# findAGoodCar
+Write a function that has 2 parameters: `cars` and `expectations`.  
+The `cars` parameter is an array of car objects.  
+The `expectations` parameter is an object.
+
+The function should check the `cars` array, if it has any objects, that are meeting the expectations of the `expectations` object.  
+We can say, that a car is meeting the expectations, and is a good car, if the car:  
+- has the same `manufacturer` as the expectations object 
+- has been made in the same year, or later, as the `yearOfManufacturing` from the expectations object  
+- has lesser km's of running, than the `km` from the expectations object  
+- is the same price, or cheaper, than the `price` from the expectations object 
+
+If the function finds no good cars, it should return `null` instead of an empty array.  
+
+example of the cars array:  
+```
+[
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2008,
+        km: 184000,
+        price: 2000000
+    },
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2011,
+        km: 212000,
+        price: 4500000
+    },
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2004,
+        km: 240000,
+        price: 1700000
+    }
+]
+```
+
+example of the expectations object:
+```
+{
+    manufacturer: "Opel",
+    yearOfManufacturing: 2000,
+    price: 30000000,
+    km: 212000
+}
+```
+
+---
+
+running example:
+```
+findAGoodCar(carsArray, {
+    manufacturer: "Audi",
+    yearOfManufacturing: 2004,
+    price: 1800000,
+    km: 325000
+})
+```
+
+should return with an array of 2 objects:
+```
+[
+  {
+    manufacturer: 'Audi',
+    yearOfManufacturing: 2004,
+    km: 289000,
+    price: 1500000
+  },
+  {
+    manufacturer: 'Audi',
+    yearOfManufacturing: 2007,
+    km: 190000,
+    price: 1750000
+  }
+]
+```
+
+---
+
+
+example of no good cars:
+```
+findAGoodCar(carsArray, {
+    manufacturer: "Audi",
+    yearOfManufacturing: 2004,
+    price: 18000,
+    km: 32500
+})
+```
+should return `null`
---
+++ test.js
@@ -0,0 +1,219 @@
+/*
+
+
+**************************************
+**************************************
+**                                  **
+**     DO NOT CHANGE THIS FILE!     **
+**                                  **
+**************************************
+**************************************
+
+
+*/
+
+const sumOfTwoNumbers = require('./sumOfTwoNumbers')
+const repeatMyself = require('./repeatMyself')
+const numberChecking = require('./numberChecking')
+const findAGoodCar = require('./findAGoodCar')
+
+const it = (desc, fn) => {
+    try {
+        fn()
+        console.log('\x1b[32m%s\x1b[0m', '\u2714 ' + desc)
+    } catch (err) {
+        console.log('\x1b[31m%s\x1b[0m', '\u2718 ' + desc)
+        //console.log(err)
+    }
+}
+
+function assert(condition) {
+    if (!condition) {
+        throw new Error()
+    }
+}
+
+console.log('\nsumOfTwoNumbers:')
+it('should return the sum of two positive integers', () => {
+    assert(sumOfTwoNumbers(1, 2) === 3)
+})
+it('should return the sum of two negative integers', () => {
+    assert(sumOfTwoNumbers(-5, -10) === -15)
+})
+
+console.log('\nrepeatMyself:')
+it('should work with simple words', () => {
+    assert(repeatMyself('hello') === "hello!hello!!hello!!!hello!!!!hello!!!!!")
+})
+it('should work with complete sentences', () => {
+    assert(repeatMyself('Hello World!') === "Hello World!!Hello World!!!Hello World!!!!Hello World!!!!!Hello World!!!!!!")
+})
+it('should work with empty string', () => {
+    assert(repeatMyself("") === "!!!!!!!!!!!!!!!")
+})
+
+console.log('\nnumberChecking:')
+it('should return \"sok\" from a not whole number that is greater than 10', () => {
+    assert(numberChecking(10.1) === "sok")
+})
+it('should return \"sok\" from an integer greater than 10', () => {
+    assert(numberChecking(11) === "sok")
+})
+it('should return 0 when a negative number is given', () => {
+    assert(numberChecking(-1) === 0)
+})
+it('should return 0 when a not whole negative number is given', () => {
+    assert(numberChecking(-0.1) === 0)
+})
+it('should return the number rounded down when 0 < number < 10 and the number is not an integer', () => {
+    assert(numberChecking(9.9) === 9)
+})
+it ('should return the number when 0 < number < 10 and the number is an integer', () => {
+    assert(numberChecking(5) === 5)
+})
+
+const carsArray = [
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2008,
+        km: 184000,
+        price: 2000000
+    },
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2011,
+        km: 212000,
+        price: 4500000
+    },
+    {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2004,
+        km: 240000,
+        price: 1700000
+    },
+    {
+        manufacturer: "BMW",
+        yearOfManufacturing: 2002,
+        km: 280000,
+        price: 2400000
+    },
+    {
+        manufacturer: "BMW",
+        yearOfManufacturing: 2014,
+        km: 80000,
+        price: 11000000
+    },
+    {
+        manufacturer: "BMW",
+        yearOfManufacturing: 2010,
+        km: 140000,
+        price: 8000000
+    },
+    {
+        manufacturer: "Tesla",
+        yearOfManufacturing: 2021,
+        km: 110000,
+        price: 27000000
+    },
+    {
+        manufacturer: "Tesla",
+        yearOfManufacturing: 2019,
+        km: 40000,
+        price: 31000000
+    },
+    {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2001,
+        km: 280000,
+        price: 1200000
+    },
+    {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2004,
+        km: 289000,
+        price: 1500000
+    },
+    {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2007,
+        km: 190000,
+        price: 1750000
+    },
+    {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2011,
+        km: 212000,
+        price: 3200000
+    }
+]
+
+console.log('\nfindAGoodCar:')
+it('should return null if no good car could be found', () => {
+    assert(findAGoodCar(carsArray, { }) === null)
+})
+it('should return all BMW cars if all is matching the requirements', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "BMW",
+        yearOfManufacturing: 2000,
+        price: 50000000,
+        km: 500000
+    }).length === 3)
+})
+it('should filter correctly by year of manufacturing', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2005,
+        price: 50000000,
+        km: 500000
+    }).length === 2)
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2004,
+        price: 50000000,
+        km: 500000
+    }).length === 3)
+})
+it('should filter correctly by price', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Tesla",
+        yearOfManufacturing: 2015,
+        price: 30000000,
+        km: 5000000
+    }).length === 1)
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Tesla",
+        yearOfManufacturing: 2015,
+        price: 31000000,
+        km: 5000000
+    }).length === 2)
+})
+it('should filter correctly by kilometers', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2000,
+        price: 30000000,
+        km: 212000
+    }).length === 1)
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Opel",
+        yearOfManufacturing: 2000,
+        price: 30000000,
+        km: 212001
+    }).length === 2)
+})
+it('should return one car if only one is matching the requirements', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2004,
+        price: 1800000,
+        km: 200000
+    }).length === 1)
+})
+it('should return two cars if two cars if matching the requirements', () => {
+    assert(findAGoodCar(carsArray, {
+        manufacturer: "Audi",
+        yearOfManufacturing: 2001,
+        price: 1500000,
+        km: 300000
+    }).length === 2)
+})
